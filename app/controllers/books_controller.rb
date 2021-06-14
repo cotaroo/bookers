2477bc0e2c1462aef52d9c4d537029c2b1de9755
cotaroo <cotaro.50tal@gmail.com>
2021-06-14 06:18:21 +0000

    #16 update edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,24 +3,32 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
 
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(:id)
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
-  
+
   private
   
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
